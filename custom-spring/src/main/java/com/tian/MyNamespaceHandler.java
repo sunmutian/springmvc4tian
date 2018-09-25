ec98883308074e2b562ef4adb0b2dabf95daaea8
lawt <251965157@qq.com>
2018-09-25 10:12:04 +0800

    类添加注释

--- custom-spring/src/main/java/com/tian/MyNamespaceHandler.java
+++ custom-spring/src/main/java/com/tian/MyNamespaceHandler.java
@@ -1,7 +1,12 @@
 package com.tian;
 
 import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
-
+/**
+ * MyNamespaceHandler class
+ *
+ * @author Java后端技术栈
+ * @date 2018/09/25
+ */
 public class MyNamespaceHandler extends NamespaceHandlerSupport {
     @Override
     public void init() {
--- custom-spring/src/main/java/com/tian/PeopleBeanDefinitionParser.java
+++ custom-spring/src/main/java/com/tian/PeopleBeanDefinitionParser.java
@@ -4,7 +4,12 @@ import org.springframework.beans.factory.support.BeanDefinitionBuilder;
 import org.springframework.beans.factory.xml.AbstractSingleBeanDefinitionParser;
 import org.springframework.util.StringUtils;
 import org.w3c.dom.Element;
-
+/**
+ * PeopleBeanDefinitionParser class
+ *
+ * @author Java后端技术栈
+ * @date 2018/09/25
+ */
 public class PeopleBeanDefinitionParser extends
         AbstractSingleBeanDefinitionParser {
     @Override
