e52b8f4b9502e12160ada3fa96ccac0ce89acee7
lawt <251965157@qq.com>
2018-12-12 15:40:29 +0800

    添加spring-cloud-demo工程

--- spring-cloud-demo/spring-cloud-client/src/main/java/com/tian/springcloudclient/SpringCloudClientApplication.java
+++ spring-cloud-demo/spring-cloud-client/src/main/java/com/tian/springcloudclient/SpringCloudClientApplication.java
@@ -1,13 +1,14 @@
-package com.tian.springcloudserver;
+package com.tian.springcloudclient;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class SpringCloudServerApplication {
+public class SpringCloudClientApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringCloudServerApplication.class, args);
+		SpringApplication.run(SpringCloudClientApplication.class, args);
 	}
 }
